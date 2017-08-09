dba11d1231f0f28bb31b9a39669330403a382a4d
Alexis Low <alexisthelow@gmail.com>
2017-08-09 13:43:19 -0600

    study drill

--- RunningTotal.java
+++ RunningTotal.java
@@ -12,7 +12,8 @@ public class RunningTotal {
 			System.out.print("Value: ");
 			current = keyboard.nextInt();
 			total += current;
-			System.out.println("The total so far is: " + total);
+			if (current !=0)
+				System.out.println("The total so far is: " + total);
 		} while (current != 0);
 		
 		System.out.println("The final total is: " + total);
